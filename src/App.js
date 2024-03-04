1b563a907f13d9d9aa6a14c7bb80f4a0b60574cb
Yash Dhameliya <56759345+yashdhameliya88@users.noreply.github.com>
2024-03-04 19:21:22 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 import "./CSS/style.css";
 
 function App() {
-  const [num, setNum] = useState(0);
+const [num, setNum] = useState(0);
 
-  function inc(n){
-    setNum (num + n);
-  }
+function inc(n){
+  setNum(num + n);
+}
 
-  function dec(n){
-    setNum (num - n);
-  }
-
+function dec(n){
+  setNum(num - n)
+}
   return (
     <div className="main">
       <h1>{num}</h1>
-      <div className="myBtn">
-        <button className="btn" onClick={() => inc(2)}>Increment</button>
-        <button className="btn" onClick={() => dec(1)}>Decrement</button>
-      </div>
+      <button className="btn" onClick={() => inc(1)}>Increment</button>
+      <button className="btn" onClick={() => dec(1)}>Decrement</button>
     </div>
   );
 }
