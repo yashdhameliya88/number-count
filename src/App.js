4698dee88baba90cd597eaa5f916cad78f61b370
Yash Dhameliya <56759345+yashdhameliya88@users.noreply.github.com>
2024-03-04 18:41:35 +0530

    Number Increment and Decrement
    Using useState Hook with callback function...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import "./CSS/style.css";
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  function inc(n){
+    setNum (num + n);
+  }
+
+  function dec(n){
+    setNum (num - n);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>{num}</h1>
+      <div className="myBtn">
+        <button className="btn" onClick={() => inc(2)}>Increment</button>
+        <button className="btn" onClick={() => dec(1)}>Decrement</button>
+      </div>
     </div>
   );
 }
